28540142f5b7abb0571e979719b66c5f52289fff
Arkadiusz Adamczak <arkadiusz.adamczak@gmail.com>
2024-01-29 20:27:44 +0100

    Added maven profiles & swagger

--- modules/identity/identity-infrastructure/identity-infrastructure-jpa/src/main/java/pl/harpi/janus/identity/infrastructure/jpa/user/UserJpaAdapter.java
+++ modules/identity/identity-infrastructure/identity-infrastructure-jpa/src/main/java/pl/harpi/janus/identity/infrastructure/jpa/user/UserJpaAdapter.java
@@ -1,17 +1,14 @@
 package pl.harpi.janus.identity.infrastructure.jpa.user;
 
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.harpi.janus.identity.domain.model.user.User;
 import pl.harpi.janus.identity.domain.ports.outbound.user.UserPort;
 import pl.harpi.janus.identity.infrastructure.jpa.user.mapper.UserJpaMapper;
 import pl.harpi.janus.identity.infrastructure.jpa.user.model.UserEntity;
-import pl.harpi.lib.core.infrastructure.Profiles;
 import pl.harpi.lib.core.infrastructure.jpa.JpaAdapter;
 
 @Repository
-@Profile(Profiles.JPA)
 public class UserJpaAdapter extends JpaAdapter<UserEntity, User, String> implements UserPort {
   private final UserJpaRepository repository;
 
