4df225e5e0cb57e6094f666e20ef552d55a29f46
Arkadiusz Adamczak <arkadiusz.adamczak@gmail.com>
2024-02-10 11:24:36 +0100

    Introduced profiles dev and release

--- app/src/main/java/pl/harpi/janus/JanusApplication.java
+++ app/src/main/java/pl/harpi/janus/JanusApplication.java
@@ -2,19 +2,13 @@ package pl.harpi.janus;
 
 import java.time.ZoneId;
 import java.util.TimeZone;
-
-import lombok.val;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import pl.harpi.lib.core.infrastructure.Profiles;
 
 @SpringBootApplication
 class JanusApplication {
   public static void main(String[] args) {
     TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Europe/Warsaw")));
-
-    val application = new SpringApplication(JanusApplication.class);
-    application.setAdditionalProfiles(Profiles.JPA);
-    application.run(args);
+    SpringApplication.run(JanusApplication.class, args);
   }
 }
