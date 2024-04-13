037f65e9835bfd6d868eaa0e206a1066fd5985b7
Arkadiusz Adamczak <arkadiusz.adamczak@gmail.com>
2024-04-13 19:22:47 +0200

    Upgrade spring-boot-parent to version 1.0.4

--- modules/identity/identity-domain-impl/src/main/java/pl/harpi/janus/identity/domain/services/user/AbstractUserService.java
+++ modules/identity/identity-domain-impl/src/main/java/pl/harpi/janus/identity/domain/services/user/AbstractUserService.java
@@ -1,13 +1,12 @@
 package pl.harpi.janus.identity.domain.services.user;
 
 import com.google.common.base.Preconditions;
+import java.util.Optional;
 import pl.harpi.janus.identity.domain.model.user.User;
 import pl.harpi.janus.identity.domain.ports.outbound.user.UserPort;
-import pl.harpi.lib.core.domain.AbstractService;
+import pl.harpi.lib.core.domain.AbstractDtoService;
 
-import java.util.Optional;
-
-public abstract class AbstractUserService extends AbstractService<User, String, UserPort> {
+public abstract class AbstractUserService extends AbstractDtoService<User, String, UserPort> {
     protected Optional<User> findByUsername(String username) {
         Preconditions.checkNotNull(username, "Username cannot be null");
         Preconditions.checkArgument(!username.isEmpty(), "Username cannot be empty");
